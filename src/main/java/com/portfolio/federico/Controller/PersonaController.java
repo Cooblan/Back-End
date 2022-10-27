2da90ecfefa0f39925ad7ce2b435c05a129f1c33
Cooblan <fx.silva01@gmail.com>
2022-10-27 16:37:59 -0300

    Back end listo

--- src/main/java/com/portfolio/federico/Controller/PersonaController.java
+++ src/main/java/com/portfolio/federico/Controller/PersonaController.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,6 +68,7 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
     } */
     
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtopersona){
         if(!personaService.existsById(id)){
